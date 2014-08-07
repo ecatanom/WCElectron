dd974698034f5baca1cfd9b1faf7fcd678429106
Erika Catano Mur <ecatanom@lbnegpvm01.fnal.gov>
2014-08-06 22:10:31 -0500

    First commit

---
+++ macros/CalcErr_C.d
@@ -0,0 +1,4 @@
+
+# DO NOT DELETE
+./CalcErr_C.so: /lbne/app/users/trj/wcsimsoft/ROOT/root/include/cintdictversion.h /lbne/app/users/trj/wcsimsoft/ROOT/root/include/RVersion.h
+CalcErr_C__ROOTBUILDVERSION= 5.30/00
